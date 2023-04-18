d724b28d2404ff5d5285e2047953756535746814
unknown <thakuramitcomp@gmail.com>
2023-04-18 01:16:46 +0100

    ADded new req parms

--- src/main/java/com/pythoncall/jpython/controller/MainController.java
+++ src/main/java/com/pythoncall/jpython/controller/MainController.java
@@ -5,20 +5,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
-  @RequestMapping(value = "/callingPythonFromJava", method = RequestMethod.GET)
-  public String callPython() {
-    try {
-      return Main.callingPythonFromJava("coma", "coma", "coma", "coma", "coma");
-    } catch (Exception e) {
-      LOGGER.error("Exception in calling ", e);
-      return "ExceptionWhileCallingPython";
+    @RequestMapping(value = "/callingPythonFromJava", method = RequestMethod.GET)
+    public String callPython(@RequestParam String symptom1, @RequestParam(defaultValue = "coma", required = false) String symptom2,
+                             @RequestParam(defaultValue = "coma", required = false) String symptom3, @RequestParam(defaultValue = "coma", required = false) String symptom4,
+                             @RequestParam(defaultValue = "coma", required = false) String symptom5) {
+        try {
+            return Main.callingPythonFromJava(symptom1, symptom2, symptom3, symptom4, symptom5);
+        } catch (Exception e) {
+            LOGGER.error("Exception in calling ", e);
+            return "ExceptionWhileCallingPython";
+        }
     }
-  }
 }
